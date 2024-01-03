6fb8ef75e904775aeff2df8b539040a5652dbef7
Kenji Wada <ch3cooh393@gmail.com>
2024-01-03 11:52:18 +0900

    update

--- SampleOpenCV/OpenCVSample/ContentView.swift
+++ SampleOpenCV/OpenCVSample/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var image = UIImage(named: "sample")
     @State private var text: String?
-    
+
     var body: some View {
         VStack {
             if let image = image {
@@ -31,22 +31,22 @@ struct ContentView: View {
         }
         .padding()
     }
-    
+
     private func action() {
         guard let source = UIImage(named: "sample") else {
             text = "画像の取得に失敗"
             return
         }
-        
+
         guard let image = ImageProcessor.convert(toGrayscale: source) else {
             text = "画像の変換に失敗"
             return
         }
-        
+
         self.image = image
         text = nil
     }
-    
+
     private func clear() {
         image = UIImage(named: "sample")
         text = nil
