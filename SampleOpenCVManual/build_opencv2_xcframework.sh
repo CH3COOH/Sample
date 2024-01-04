adf2b5287d79eaf1a375fb83dae320b216e1fdad
Kenji Wada <ch3cooh393@gmail.com>
2024-01-04 09:22:56 +0900

    update

---
+++ SampleOpenCVManual/build_opencv2_xcframework.sh
@@ -0,0 +1,12 @@
+OPENCV_VERSION=4.9.0
+SOURCE_URL=https://github.com/opencv/opencv/archive/refs/tags/$OPENCV_VERSION.zip
+WORKSPACE_DIR=/Users/ch3cooh/works/opencv
+
+mkdir -p "$WORKSPACE_DIR/opencv-$OPENCV_VERSION"
+
+curl -L $SOURCE_URL -o "$WORKSPACE_DIR/$OPENCV_VERSION.zip"
+unzip "$WORKSPACE_DIR/$OPENCV_VERSION.zip" -d "$WORKSPACE_DIR"
+
+cd "$WORKSPACE_DIR/opencv-$OPENCV_VERSION"
+
+python ./platforms/apple/build_xcframework.py --out ./build --iphoneos_archs arm64 --iphonesimulator_archs arm64 --without videoio --iphoneos_deployment_target 14 --build_only_specified_archs
